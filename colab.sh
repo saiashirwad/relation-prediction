6758711c956d56fe8074dd570dd3dbb8c4adac29
Sai Ashirwad <saiashirwadr@gmail.com>
2020-07-15 10:40:46 -0500

    lol

--- colab.sh
+++ colab.sh
@@ -3,4 +3,5 @@ pip install livelossplot --quiet
 cd openke
 rm -rf release
 bash make.sh
-cd /content/drive/My drive/code/relation-prediction
+cd ..
+# cd /content/drive/My drive/code/relation-prediction
