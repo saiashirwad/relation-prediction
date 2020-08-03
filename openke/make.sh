82bb0bea06e9d4c7fd98ff83aeee96608c418f40
Sai Ashirwad R <saiashirwadr@gmail.com>
2020-08-04 03:04:09 +0530

    ehh

--- openke/make.sh
+++ openke/make.sh
@@ -1,2 +1,2 @@
 mkdir release
-g++ ./base/Base.cpp -fPIC -shared -o ./release/Base.so -pthread -O3 -march=native
+g++ ./base/Base.cpp -fPIC -shared -o ./release/Base_Linux.so -pthread -O3 -march=native
